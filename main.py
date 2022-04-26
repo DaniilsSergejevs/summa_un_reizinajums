daff40bed25b29c0e167f8fc53a0abede72e90f3
Daniil Sergejev <77155713+DaniilsSergejevs@users.noreply.github.com>
2022-04-26 08:12:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Ievadi divi skaitli:')
+a = int(input('Ievadi pirmo skaitli: '))
+b = int(input('Ievadi otro skaitli: '))
+if a>=b:
+  print('Summa ir: ', a+b)
+else: print('Reizinājums ir: ', a*b)
